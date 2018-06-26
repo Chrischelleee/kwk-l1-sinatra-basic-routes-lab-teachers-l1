42f983eef0b3263a7cf64c07c5b680a4c9df90e1
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-26 13:08:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Chrischelle"
+  end
+
+  get '/hometown' do
+    "My hometown is Raleigh NC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Young Blood"
+  end
 end
